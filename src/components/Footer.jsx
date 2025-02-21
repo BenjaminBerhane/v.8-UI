bec06d741a0512d3874471b6eb5c329e64a1b398
Gustav Thilander <gustav.thilander@gmail.com>
2025-02-21 12:20:52 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,19 @@
 
 const Footer = () => {
   return (
-    <footer className='p-2' ><h1>Kontakta oss</h1></footer>
-  )
+    <footer className="bg-gray-800 text-white p-4 fixed bottom-0 w-full">
+      <div className="container mx-auto text-center">
+        <h1 className="text-xl font-bold mb-2">Kontakta oss</h1>
+        <p className="mb-4">Email: info@nyhetssida.se | Telefon: 012-3456789</p>
+        <div className="flex justify-center space-x-4">
+          <a href="#" className="text-gray-400 hover:text-white">Facebook</a>
+          <a href="#" className="text-gray-400 hover:text-white">Twitter</a>
+          <a href="#" className="text-gray-400 hover:text-white">Instagram</a>
+        </div>
+        <p className="mt-4 text-gray-500">&copy; 2025 Nyhetssida. Alla rättigheter förbehållna.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
