81b54d232f9c940396186f4f5ee96076f6786169
BenjaminBerhane <benjamin.berhane@chasacademy.se>
2025-02-21 13:01:04 +0100

    fixat lite i listan

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 Vi tänkte skapa en nyhetssida eller något liknande. Vi valde Tailwind som UI-ramverk för att den var kompatibel med React och verkade lite lättare att komma igång med. Vi började med att skapa en figma Wireframe och gick sedan vidare med själva koden. 
+Vi fick en hel del problem med Tailwind och hade inte så mycket tid att undersöka det så vi kom inte så långt.
+
+Figma: https://www.figma.com/design/53tWW1Mo13APvrqzdK7eVn/boiler-room-vecka-8?node-id=0-1&t=KuszeuYospD5u5YN-1
+
 
 ##Installation
 Klona projektet:
 
-git clone https://github.com/d.git
-cd ditt-repo
+git clone https://github.com/v.8-UI.git
+cd v.8-UI
 
 
 Installera beroenden:
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,61 +1,47 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
-
-const features = [
+const newsArticles = [
   {
-    name: 'Push to deploy.',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
+    id: 1,
+    title: 'Apple announces new iPhone 14 Pro',
+     paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    
   },
   {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
+    id:2,
+    title: 'Google announces new Android 13 features',
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
+    id:3, 
+    title: 'Tesla announces new Cybertruck',
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
 ]
 
 export default function Aside() {
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pt-4 lg:pr-8">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-                A better workflow
-              </p>
-              <p className="mt-6 text-lg/8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
-              </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon aria-hidden="true" className="absolute top-1 left-1 size-5 text-indigo-600" />
-                      {feature.name}
-                    </dt>{' '}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
+    <ul role="list" className="divide-y divide-gray-100">
+      {newsArticles.map((article) => (
+        <li key={article.id} className="flex justify-between gap-x-6 py-5">
+          <div className="flex min-w-0 gap-x-4">
+            <img alt="" src={article.imageUrl} className="size-12 flex-none  bg-gray-50" />
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm/6 font-semibold text-gray-900">{article.title}</p>
             </div>
           </div>
-          <img
-            alt="Product screenshot"
-            src="https://tailwindui.com/plus-assets/img/component-images/dark-project-app-screenshot.png"
-            width={2432}
-            height={1442}
-            className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-          />
-        </div>
-      </div>
-    </div>
+
+          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+            
+            {article.lastSeen ? (
+              <p className="mt-1 text-xs/5 text-gray-500">
+                {article.paragraph}
+              </p>
+            ) : (
+              <div className="mt-1 flex items-center gap-x-1.5">
+              </div>
+            )}
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
